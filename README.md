8b0ca8deb976dd2221673e886c92d6472efb8424
Megha Murthy <m.megha21@gmail.com>
2020-04-17 01:43:52 -0700

    Before heroku all files

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # budget-tracker
 Progressive Web App
+
+## Description:
+This application allows the user to access the website for offline.
+
+
+## Usage:
+
+The user will be able to add expenses and deposits to their budget with or without a connection. When entering transactions offline, they will be populated the total when brought back online.
+
+1. Offline Functionality:
+
+- Enter deposits offline
+
+- Enter expenses offline
+
+- When brought back online:
+
+Offline entries are added to tracker.
+
+
+## Technologies:
+
+HTML,CSS,Javascript,NodeJS, Mongo/Mongoose, PWA
+
+
+
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = 3008;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/offlinebudget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
